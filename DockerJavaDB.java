0f9b3146ac089986880e5317badf33e129e8432a
jerzyantoniuk <74658633+jerzyantoniuk@users.noreply.github.com>
2020-11-28 14:26:11 +0100

    Update DockerJavaDB.java

--- DockerJavaDB.java
+++ DockerJavaDB.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 public class DockerJavaDB {
 
     static String url = "jdbc:mysql://10.0.10.3:3306/full";
-
+    static String driver="com.mysql.jdbc.Driver"'
 
     public static void main(String[] args) {
         // write your code here
-
+          Class.forName(driver);
         Scanner scanner= new Scanner(System.in);
         CreateTable();
         while(true) {
